86e1df54ba42cab900cccef812debb0b7b9dddf6
Jaime Melis <jmelis@opennebula.org>
2013-04-05 02:30:16 +0200

    add comments, fix the qemu patching and start services

--- loadays-opennebula.sh
+++ loadays-opennebula.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# make sunstone listen in 0.0.0.0
 sed -i 's/^:host.*/:host: 0.0.0.0/' /etc/one/sunstone-server.conf
+
+# clear firewall
 iptables -F
+
+# disable SELinux
 setenforce 0
+
+# download QEMU drivers
 curl -s http://dev.opennebula.org/attachments/download/663/qemu.tar.gz \
     | sudo -u oneadmin tar xzf - -C /var/lib/one/remotes/vmm/
-sed -i 's/ kvm"/ qemu"/' /etc/one/oned.conf
+sed -i 's/-r 0 kvm/-r 0 qemu/' /etc/one/oned.conf
+
+# start required services
+service messagebus start
+service libvirtd start
